41f249cab118692d6a3d232efc0e36e78ebc3ba7
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2025-10-05 18:29:11 +0200

    menu nawigacyjne

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('start');
 Route::get('/kontakt', function () {
     return view('kontakt');
-});
-Route::get('/onas', function () {
-    return view('onas');
+})->name('kontakt');
+Route::get('/o-nas', function () {
+    $zadania =[
+        'Zadanie 1',
+        'Zadanie 2',
+        'Zadanie 3'
+    ];
+    $tasks =[
+        'Task 1',
+        'Task 2',
+        'Task 3'
+    ];
+    /* return view('onas', ['zadania'=> $zadania]); */
+    /* return view('onas')->with('zadanka',$zadania); */
+    return view('onas', compact('zadania', 'tasks'));
+})->name('onas');
+Route::get('/test', function () {
+    return "Test";
 });
